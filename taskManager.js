81f774f16f4290b15e3a985749b311bb8753e9cb
Ivana Jones <54974353+ivasve@users.noreply.github.com>
2020-08-27 11:26:48 +1000

    Update taskManager.js

--- taskManager.js
+++ taskManager.js
@@ -1,3 +1,4 @@
+// import Task from "./taskManager";
 class Task {
   constructor(id, name, description, assignedTo, dueDate, status) {
     this.id = id;
@@ -15,35 +16,48 @@ class Task {
 class TaskManager {
   constructor() {
     this.tasks = [];
-    //to do - get tasks from local storage
-
     this.taskId = 0;
+    this.DisplayStatus = "ALL";
   }
 
   saveTasks() {
-    window.localStorage.setItem("tasks", JSON.stringify(this.tasks));
+    //alert("saving tasks");
+    try {
+      window.localStorage.setItem("tasks", JSON.stringify(this.tasks));
+      console.table("After Save: ");
+      console.table(this.tasks);
+    } catch (error) {
+      alert("Local Storage Save Tasks Error" + error);
+    }
   }
 
   readTasks() {
-    let storedTasksJSON = window.localStorage.getItem("tasks");
-    let storedTaskObjects = JSON.parse(storedTasksJSON);
+    try {
+      let storedTasksJSON = window.localStorage.getItem("tasks");
+      let storedTaskObjects = JSON.parse(storedTasksJSON);
+      this.tasks = [];
+      this.taskId = 0;
+      if (storedTaskObjects) {
+        storedTaskObjects.forEach((t) => {
+          const taskObject = new Task(
+            this.taskId,
+            t.name,
+            t.description,
+            t.assignedTo,
+            new Date(t.dueDate),
+            t.status
+          );
 
-    if (storedTaskObjects) {
-      storedTaskObjects.forEach((t) => {
-        const taskObject = new Task(
-          t.id,
-          t.name,
-          t.description,
-          t.assignedTo,
-          t.dueDate,
-          t.status
-        );
+          this.tasks.push(taskObject);
+          this.taskId++;
+        });
+      }
+      console.table("After Read: ");
 
-
-        this.tasks.unshift(taskObject);
-        this.taskId++;
-      })
-    };
+      console.table(this.tasks);
+    } catch (error) {
+      alert("Local Storage Read Tasks Error" + error);
+    }
   }
   getTasks() {
     return this.tasks;
@@ -54,28 +68,32 @@ class TaskManager {
       return task.status == status;
     });
   }
+
   addTask(newTask) {
     this.taskId++;
+
     newTask.id = this.taskId;
-    this.tasks.unshift(newTask);
+    // alert(newTask.id);
+    this.tasks.push(newTask);
     this.saveTasks();
+    this.readTasks();
     return this.taskId;
   }
+
   deleteTask(taskToDelete) {
     let taskIndex = this.tasks.findIndex(function (task) {
       return task.id == taskToDelete.id;
     });
-    //alert(taskIndex);
     if (taskIndex != -1) {
       let deletedItem = this.tasks.splice(taskIndex, 1);
       this.saveTasks();
     }
   }
+
   deleteTaskById(taskId) {
     let taskIndex = this.tasks.findIndex(function (task) {
       return task.id == taskId;
     });
-    //alert(taskIndex);
     if (taskIndex != -1) {
       let deletedItem = this.tasks.splice(taskIndex, 1);
       this.saveTasks();
@@ -102,6 +120,23 @@ class TaskManager {
     }
     console.log(this.tasks);
   }
+
+  updateTask(taskId, name, description, assignedTo, dueDate, status) {
+    for (let i = 0; i < this.tasks.length; i++) {
+      if (this.tasks[i].id === taskId) {
+        this.tasks[i].name = name;
+        this.tasks[i].description = description;
+        this.tasks[i].assignedTo = assignedTo;
+        this.tasks[i].dueDate = dueDate;
+        this.tasks[i].status = status;
+      }
+    }
+    this.saveTasks();
+    this.readTasks();
+    let tasksRow = document.getElementById("tasksrow");
+    this.displayTasks(tasksRow);
+  }
+
   assignTask(taskId, newAssignee) {
     let taskToUpdate = null;
     taskToUpdate = this.getTaskFromId(taskId);
@@ -111,159 +146,167 @@ class TaskManager {
     console.log(this.tasks);
   }
 
-  randomTask(id) {
-    const name = [
-      "Buy groceries",
-      "Go for a walk",
-      "Prepare taxes",
-      "Doctors appointment",
-      "Attend show",
-      "Watch movie",
-      "Feed Dog",
-      "Do laundry",
-      "Test code",
-      "Paint fence",
-      "Inspect car",
-      "Register car",
-      "Insure car",
-      "Ring Aunt Jane",
-    ];
-    const status = ["TODO", "INPROGRESS", "REVIEW", "DONE"];
-    const person = ["Kriss", "Ivana"];
-    let ran = Math.floor(Math.random() * name.length);
+  displayTasks(destinationElement) {
+    let out = destinationElement;
 
-    let statusRandom = Math.floor(Math.random() * 4);
-    let personRandom = Math.floor(Math.random() * 2);
-
-    let newTask = new Task(
-      id,
-      name[ran],
-      name[ran],
-      person[personRandom],
-      new Date(2020, 2, 1 + id, id),
-      status[statusRandom]
-    );
-    return newTask;
-  }
-  generateRandomTasks(shouldSave) {
-    for (let i = 0; i < 3; i++) {
-      let randomTask = this.randomTask(i);
-      this.addTask(randomTask);
+    if (out) {
+      out.innerHTML = "";
     }
-    console.table(this.tasks);
-    if (shouldSave) {
-      this.saveTasks();
-    }
-  }
-  displayTasks(destinationId) {
-    let out = document.getElementById(destinationId);
-    out.innerHTML = "";
 
-    this.tasks.forEach((theTask) => {
-      domManager.createTaskCardDomElements(destinationId, theTask);
+    let tasksToDisplay = this.getTasksWithStatus(this.DisplayStatus);
+
+    if (this.DisplayStatus == "ALL") {
+      tasksToDisplay = this.tasks;
+    }
+
+    tasksToDisplay.forEach((theTask) => {
+      this.createTaskCardDomElements(destinationElement, theTask);
     });
   }
-}
 
-const domManager = {
-  createTaskCardDomElements: function (destinationId, task) {
+  createTaskCardDomElements(destinationElement, task) {
+    //console.log("createTaskCardDomElements");
+    if (!destinationElement) {
+      return;
+    }
     const html = `
-        <div id="${task.id}" class="task col-lg-4">
-          <div class="card my-4">
-    
-            <div
-              class="card-header"
-              id="header1
-              >
-              <h2 class="mb-0 text-left" style="text-decoration: none;">
-                <button
-                  id="b1"
-                  class="btn btn-block text-left"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="#collapse1"
-                  aria-expanded="true"
-                  aria-controls="collapse1"
-                  style="background-color: rgb(96, 96, 245); color: white;"
-                >
-                  <strong>
-                    <h5
-                      class="text-center"
-                      style="
-                        text-decoration: none;
-                        background-color: rgb(96, 96, 245);
-                        color: white;
-                      "
-                    >
-                    ${task.name}</h5>
-                  </strong>
-                </button>
-              </h2>
-            </div>
-    
-            <div id="collapse1" class="collapse show" aria-labelledby="head1">
-              <div class="card-body">
-                <h5 class="card-title">${task.description}</h5>
-                <p class="card-text"></p>
-              </div>
-    
-              <ul class="list-group list-group-flush">
-                <li
-                  class="list-group-item"
-                  style="background-color: rgb(141, 234, 250);"
-                >
-                  Asignee: ${task.assignedTo}
-                </li>
-                <li class="list-group-item">Date Due: ${task.dueDate}</li>
-                <li class="list-group-item status">Status: ${task.status}</li>
-                <li class="list-group-item">
-                  <button
-                    type="button"
-                    class="btn btn-primary"
-                    data-toggle="modal"
-                    data-target="#taskEditModal"
+            <div id="${task.id}" class="task col-lg-4">
+              <div class="card my-4">
+        
+                <div
+                  class="card-header"
+                  id="header1
                   >
-                    Edit
-                  </button>
-                  <button
+                  <h2 class="mb-0 text-left" style="text-decoration: none;">
+                    <button
+                      id="b1"
+                      class="btn btn-block text-left cardNameButton ${
+                        task.status
+                      }"
                       type="button"
-                      id="deleteBtn${task.id}"
-                      class="deleteBtn btn btn-secondary btn-sm"
+                      data-toggle="collapse"
+                      data-target="#collapse1"
+                      aria-expanded="true"
+                      aria-controls="collapse1"
+                     
+                    >
+                      <strong>
+                        <h5
+                        
+                         class="cardName text-center"
+                        >
+                        ${task.name}</h5>
+                      </strong>
+                    </button>
+                  </h2>
+                </div>
+        
+                <div id="collapse1" class="collapse show" aria-labelledby="head1">
+                  <div class="card-body">
+                    <h5 class="card-title">${task.description}</h5>
+                    <p class="card-text"></p>
+                  </div>
+        
+                  <ul class="list-group list-group-flush">
+                    <li
+                      class="list-group-item"
+                      style="background-color: rgb(141, 234, 250);"
+                    >
+                      Asignee: ${task.assignedTo}
+                    </li>
+                    <li class="list-group-item">Date Due: ${task.dueDate.toLocaleDateString()}</li>
+                    <li class="list-group-item status">Status: ${
+                      task.status
+                    }</li>
+                    <li class="list-group-item">
+                      <button
+                        type="button"
+                        id="editBtn${task.id}"
+                        class="btn btn-primary"
+                        data-toggle="modal"
+                        data-target="#taskEditModal"
                       >
-                      Delete
-                  </button>
-                </li>
-              </ul>
+                        Edit
+                      </button>
+                      <button
+                          type="button"
+                          id="deleteBtn${task.id}"
+                          class="deleteBtn btn btn-secondary btn-sm"
+                          >
+                          Delete
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+        
+              </div>
             </div>
-    
-          </div>
-        </div>
-      `;
-
+          `;
+    //console.log(html);
     const taskElement = document.createRange().createContextualFragment(html);
 
-    tasksContainer.append(taskElement);
+    destinationElement.append(taskElement);
+    console.log(destinationElement.innerHTML);
 
     let deleteBtn = document.querySelector("#deleteBtn" + task.id);
 
     if (deleteBtn) {
       deleteBtn.addEventListener("click", function (event) {
-        var taskElement = event.target.closest(".task");
-        taskManager.deleteTaskById(taskElement.id);
-        taskManager.displayTasks("tasksrow");
+        $(function () {
+          $("#dialog-confirm").dialog({
+            resizable: false,
+            height: "auto",
+            width: 300,
+            modal: true,
+            buttons: {
+              "Delete Task": function () {
+                var taskElement = event.target.closest(".task");
+                taskManager.deleteTaskById(taskElement.id);
+                let tasksRow = document.getElementById("tasksrow");
+                taskManager.displayTasks(tasksRow);
+
+                $(this).dialog("close");
+              },
+              Cancel: function () {
+                $(this).dialog("close");
+              },
+            },
+          });
+        });
       });
     }
-  },
-};
 
-//tests
-//const taskManager = new TaskManager();
+    let editBtn = document.querySelector("#editBtn" + task.id);
 
-//taskManager.saveTasks();
-//taskManager.readTasks();
-// let t = taskManager.addTask({ id: 20, name: 'Go fishing', description: 'Go fishing', assignedTo: 'Sid Smith', dueDate: new Date(), status: 'TODO' });
-// taskManager.deleteTask({ id: 20, name: 'Go fishing', description: 'Go fishing', assignedTo: 'Sid Smith', dueDate: new Date(), status: 'TODO' });
-// taskManager.updateTaskStatus(1, "****STATUS****");
-// taskManager.assignTask(2, "**new assignee**");
-// console.log(this.tasks);
-// console.log("finished");
+    if (editBtn) {
+      //Fill in the update form with the existing task fields
+      editBtn.addEventListener("click", function (event) {
+        var taskElement = event.target.closest(".task");
+        let task = taskManager.getTaskFromId(taskElement.id);
+        updateTaskForm.taskId.value = task.id;
+        updateTaskForm.updateTaskNameInput.value = task.name;
+        updateTaskForm.taskDescription2.value = task.description;
+        updateTaskForm.assignedTo2.value = task.assignedTo;
+        const YEAR = task.dueDate.getFullYear();
+        let month = task.dueDate.getMonth() + 1;
+        if (month < 10) {
+          month = "0" + month;
+        }
+        let day = task.dueDate.getDate();
+        if (day < 10) {
+          day = "0" + day;
+        }
+        updateTaskForm.dueDate2.value = YEAR + "-" + month + "-" + day;
+        // YEAR + "/" + MONTH + "/" + DAY;
+        updateTaskForm.status2.value = task.status;
+        // let tasksRow = document.getElementById("tasksrow");
+        // this.saveTasks();
+        // this.readTasks();
+        // taskManager.displayTasks(tasksRow);
+      });
+    }
+  }
+}
+
+exports.TaskManager = TaskManager;
+exports.task = Task;
